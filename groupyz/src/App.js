a81016f873250ec00f3699a0568248cde89e72d6
ndvisme <herespier@yahoo.com>
2023-05-22 11:52:55 +0300

    Remove photos container component from app component.

--- groupyz/src/App.js
+++ groupyz/src/App.js
@@ -1,10 +1,7 @@
-import PictureContainer from "./pages/home/PictureContainer";
-
 function App() {
   return (
     <div className="App">
       <header /> Hello World!
-      <PictureContainer />
     </div>
   );
 }
