7f6dd827a472d8d7b605a5c9ff44199820457c16
Omer Leibo <leibo.omer13@gmail.com>
2023-05-25 14:23:28 +0300

    changed headerContainer to container

--- groupyz/src/Header.jsx
+++ groupyz/src/Header.jsx
@@ -1,13 +1,18 @@
 import "./styles/Header.css";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Logo from "./assets/images/logo.svg";
 =======
 import header_logo from "./assets/images/header_logo.png";
 >>>>>>> 0fff56e (init header without buttons)
+=======
+import Header_logo from "./assets/images/Header_logo.png";
+>>>>>>> 8a2a465 (changed headerContainer to container)
 
 const Header = () => {
   return (
     <header>
+<<<<<<< HEAD
 <<<<<<< HEAD
       <div class="container">
         <div class="logoContainer">
@@ -17,6 +22,11 @@ const Header = () => {
         <div class="logoContainer">
           <img src={header_logo} alt="logo" />
 >>>>>>> 0fff56e (init header without buttons)
+=======
+      <div class="container">
+        <div class="logoContainer">
+          <img src={Header_logo} alt="logo" />
+>>>>>>> 8a2a465 (changed headerContainer to container)
         </div>
         <div class="scheduleContainer">{`Schedule messages`}</div>
       </div>
