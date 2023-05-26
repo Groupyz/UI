b383be4e4f02e28a391816624f2be1aa0d369779
ndvisme <herespier@yahoo.com>
2023-05-27 01:26:13 +0300

    After pull & merge from sandbox.

--- groupyz/src/Header.jsx
+++ groupyz/src/Header.jsx
@@ -1,40 +1,12 @@
 import "./styles/Header.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Logo from "./assets/images/logo.svg";
-=======
-import header_logo from "./assets/images/header_logo.png";
->>>>>>> 0fff56e (init header without buttons)
-=======
-import Header_logo from "./assets/images/Header_logo.png";
->>>>>>> 8a2a465 (changed headerContainer to container)
-=======
-import Logo from "./assets/images/logo.png";
->>>>>>> 841cfed (remove pixel settings for components and updated logo)
 
 const Header = () => {
   return (
     <header>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div class="container">
         <div class="logoContainer">
           <img src={Logo} width={112} height={112} alt="logo" />
-=======
-      <div class="headerContainer">
-        <div class="logoContainer">
-          <img src={header_logo} alt="logo" />
->>>>>>> 0fff56e (init header without buttons)
-=======
-      <div class="container">
-        <div class="logoContainer">
-<<<<<<< HEAD
-          <img src={Header_logo} alt="logo" />
->>>>>>> 8a2a465 (changed headerContainer to container)
-=======
-          <img src={Logo} width={112} height={112} alt="logo" />
->>>>>>> 841cfed (remove pixel settings for components and updated logo)
         </div>
         <div class="scheduleContainer">{`Schedule messages`}</div>
       </div>
