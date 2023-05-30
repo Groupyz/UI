58652a9f2bfe4ba041e527a7ba898802cff44f72
Omer Leibo <leibo.omer13@gmail.com>
2023-05-30 17:48:07 +0300

    added header and footer to app

--- groupyz/src/App.js
+++ groupyz/src/App.js
@@ -1,15 +1,32 @@
 import * as React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Homepage from "./pages/home/Homepage";
-import { Routes, Route } from "react-router-dom";
 import NoMatch from "./components/NoMatch";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
+  // all routes defined in the app
+  const routes = [
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+  ];
+
+  const location = useLocation();
+  const hideHeaderFooter = !routes.find(
+    (route) => route.path === location.pathname
+  );
+
   return (
     <div className="App">
+      {!hideHeaderFooter && <Header />}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
+      {!hideHeaderFooter && <Footer />}
     </div>
   );
 }
