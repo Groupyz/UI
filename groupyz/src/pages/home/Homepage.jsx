59e8c10d17659d1b81bd221c949e2cb92b64dd7e
Omer Leibo <leibo.omer13@gmail.com>
2023-05-31 15:25:57 +0300

    added homepage styling

--- groupyz/src/pages/home/Homepage.jsx
+++ groupyz/src/pages/home/Homepage.jsx
@@ -1,9 +1,20 @@
+import "./styles/Homepage.css";
 import PictureContainer from "./PictureContainer";
 
 const Homepage = () => (
   <>
-    <header> Hello World!</header>
-    <PictureContainer />
+    <div class="container">
+      <div class="row">
+        <div class="headlinesContainer">
+          <header> Hello World!</header>
+        </div>
+      </div>
+      <div class="row">
+        <div class="photosContainer">
+          <PictureContainer />
+        </div>
+      </div>
+    </div>
   </>
 );
 
