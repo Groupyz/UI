5d04341e0190e3e3f6b75bee26f178b0d3285e86
Omer Leibo <leibo.omer13@gmail.com>
2023-06-01 10:49:41 +0300

    cleaned App.js

--- groupyz/src/App.js
+++ groupyz/src/App.js
@@ -1,32 +1,19 @@
 import * as React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/home/Homepage";
 import NoMatch from "./components/NoMatch";
 import Header from "./Header";
 import Footer from "./Footer";
 
 function App() {
-  // all routes defined in the app
-  const routes = [
-    {
-      path: "/",
-      element: <Homepage />,
-    },
-  ];
-
-  const location = useLocation();
-  const hideHeaderFooter = !routes.find(
-    (route) => route.path === location.pathname
-  );
-
   return (
     <div className="App">
-      {!hideHeaderFooter && <Header />}
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
-      {!hideHeaderFooter && <Footer />}
+      <Footer />
     </div>
   );
 }
