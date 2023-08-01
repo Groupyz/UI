549d3e65c83de04680823582a11a204183c558c2
Omer Leibo <leibo.omer13@gmail.com>
2023-08-01 15:36:54 +0300

    added 'groups chosen' component to add groups page

--- groupyz/src/pages/add_groups/addGroups.jsx
+++ groupyz/src/pages/add_groups/addGroups.jsx
@@ -1,11 +1,16 @@
 import "./styles/addGroups.css";
 import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
 import Search from "./images/search.svg";
 import GroupsList from "./data/groupsList.jsx";
 import { useState } from "react";
+import Button_c from "../../components/global/Button_c";
+import arrow from "./images/arrow.svg";
 
 const AddGroups = () => {
   const [inputText, setInputText] = useState("");
+  const [selectedGroups, setSelectedGroups] = useState([]);
   let inputHandler = (e) => {
     //convert input text to lower case
     var lowerCase = e.target.value.toLowerCase();
@@ -23,20 +28,42 @@ const AddGroups = () => {
         </div>
         <div class="col">
           <div class="row secondRow">
-            <div class="searchBar">
-              <TextField
-                id="searchBar"
-                onChange={inputHandler}
-                variant="outlined"
-                fullWidth
-                placeholder="Search a group"
-                InputProps={{
-                  startAdornment: <img src={Search} alt="" />,
-                  disableUnderline: true,
-                }}
+            <div class="firstCol">
+              <div class="searchBar">
+                <TextField
+                  id="searchBar"
+                  onChange={inputHandler}
+                  variant="outlined"
+                  fullWidth
+                  placeholder="Search a group"
+                  InputProps={{
+                    startAdornment: <img src={Search} alt="" />,
+                    disableUnderline: true,
+                  }}
+                />
+              </div>
+              <GroupsList
+                input={inputText}
+                selectedGroups={selectedGroups}
+                setSelectedGroups={setSelectedGroups}
               />
             </div>
-            <GroupsList input={inputText} />
+            <div class="secondCol">
+              {selectedGroups.map((group) => (
+                <div className="selectedGroups">
+                  <Grid container columns={{ xs: 4 }}>
+                    <Grid item xs={4}>
+                      <Item>{group}</Item>
+                    </Grid>
+                  </Grid>
+                </div>
+              ))}
+            </div>
+            <div class="thirdCol">
+              <div class="continueButton">
+                <Button_c name=" " image={arrow} width="34px" height="34px" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
